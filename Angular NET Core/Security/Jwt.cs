cc9aeb7c2faece503443e4c084e4ba54ffcf4f30
Lucas <lucaschmelik@icloud.com>
2023-11-11 12:18:27 -0300

    Se guarda id de usuario logeado en storage

---
+++ Angular NET Core/Security/Jwt.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Claims;
+
+namespace GreenMaterialBackEnd.Security
+{
+    public class Jwt
+    {
+        public string Key { get; set; }
+        public string Issuer { get; set; }
+        public string Audience { get; set; }    
+        public string Subject { get; set; }
+
+        public static int retornaIdPorToken(ClaimsIdentity identity)
+        {
+            if (!identity.Claims.Any())
+            {
+                throw new Exception("Verificar token");
+            }
+
+            return int.Parse(identity.Claims.FirstOrDefault(x => x.Type == "id").Value);
+        }
+    }
+}
